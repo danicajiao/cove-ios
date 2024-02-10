3ea0ba79dec45d9f1c68c6025954c910e0eb6041
Daniel Cajiao <cajiao.dani@gmail.com>
2024-02-10 11:51:55 -0700

    refactor: changes splash screen and initial navigation

--- Cove/Views/SplashView.swift
+++ Cove/Views/SplashView.swift
@@ -9,35 +9,49 @@ import SwiftUI
 import FirebaseAuth
 
 struct SplashView: View {
-    @EnvironmentObject var appState: AppState
+//    @EnvironmentObject var appState: AppState
     
     var body: some View {
-        ZStack {
-            VStack {
-                Spacer()
-                Image("splash-illustration")
+        
+        GeometryReader { geo in
+            ZStack {
+                Image("splash")
                     .resizable()
                     .aspectRatio(contentMode: .fill)
-                    .frame(height: 350)
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.backdropColor)
-            .ignoresSafeArea(.all, edges: .bottom)
-            
-            Text("Cove")
-                .font(.custom("Getaway", size: 80))
-        }
-        .onAppear {
-            print(self.appState.path)
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                guard Auth.auth().currentUser != nil else {
-                    self.appState.path.append(.welcome)
-                    return
-                }
-//                self.appState.path.append(.welcome)
-                self.appState.path.append(.main)
+                    .frame(maxWidth: geo.size.width)
+                    .ignoresSafeArea()
+                Text("Cove")
+                    .font(.custom("Getaway", size: 80))
+                    .foregroundStyle(.white)
             }
         }
+        
+//        ZStack {
+//            VStack {
+//                Spacer()
+//                Image("splash-illustration")
+//                    .resizable()
+//                    .aspectRatio(contentMode: .fill)
+//                    .frame(height: 350)
+//            }
+//            .frame(maxWidth: .infinity, maxHeight: .infinity)
+//            .background(Color.backdropColor)
+//            .ignoresSafeArea(.all, edges: .bottom)
+//            
+//            Text("Cove")
+//                .font(.custom("Getaway", size: 80))
+//        }
+//        .onAppear {
+//            print(self.appState.path)
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//                guard Auth.auth().currentUser != nil else {
+//                    self.appState.path.append(.welcome)
+//                    return
+//                }
+////                self.appState.path.append(.welcome)
+//                self.appState.path.append(.main)
+//            }
+//        }
     }
 }
 

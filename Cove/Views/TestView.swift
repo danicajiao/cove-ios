d32da424a14aad430096c8da55e85a6b3110a5d8
Daniel Cajiao <cajiao.dani@gmail.com>
2025-02-27 23:39:16 -0700

    Implement `Welcome` view redesign

---
+++ Cove/Views/TestView.swift
@@ -0,0 +1,19 @@
+//
+//  TestView.swift
+//  Cove
+//
+//  Created by Daniel Cajiao on 2/25/25.
+//
+
+import SwiftUI
+import RiveRuntime
+
+struct TestView: View {
+    var body: some View {
+        Text("Test")
+    }
+}
+
+#Preview {
+    TestView()
+}
